415fe8517464f14792348ba9b38a2b856f63d3c3
mano-octavianojr <117711652+mano-octavianojr@users.noreply.github.com>
2023-07-15 23:57:03 +0800

    Update calculator.js

--- nodejs/add-only-calculator/calculator.js
+++ nodejs/add-only-calculator/calculator.js
@@ -16,6 +16,20 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get('/bmicalculator', function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post('/bmicalculator', function (req, res) {
+
+
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+
+    var bmi = (weight) / (height * height);
+    res.send("Your BMI is: " + bmi);
+});
+
 app.post('/', function (req, res) {
 
     var num1 = Number(req.body.num1);
