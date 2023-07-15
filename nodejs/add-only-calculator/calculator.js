d20afc48e37e4cab0d8efc7407291d865d4cb9b6
mano-octavianojr <117711652+mano-octavianojr@users.noreply.github.com>
2023-07-15 23:03:34 +0800

    calculator that can add 2 numbers

---
+++ nodejs/add-only-calculator/calculator.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// NOTE: Codes below are used to replicate the func of __dirname.
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// - - - - - 
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', function (req, res) {
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+
+    res.send("the sum of the values is: " + (num1 + num2));
+});
+
+app.listen(3000, function () {
+    console.log('listening in port 3000')
+})
